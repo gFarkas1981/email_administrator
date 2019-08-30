7eabcbc99111f660b0b2ae0f3740562bca4dc4f7
gFarkas1981 <info@gfarkas.com>
2019-08-30 18:35:06 +0200

    First Commit

--- src/com/gfarkas/emailapp/Email.java
+++ src/com/gfarkas/emailapp/Email.java
@@ -2,10 +2,22 @@ package com.gfarkas.emailapp;
 
 public class Email {
 
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
 
+    // Constructor to receive the first name and last name
 
-        String firstName;
-        String lastName;
+    // Ask for department
 
+    // Generate a random password
 
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
 }
--- src/com/gfarkas/emailapp/EmailApp.java
+++ src/com/gfarkas/emailapp/EmailApp.java
@@ -1,4 +1,9 @@
 package com.gfarkas.emailapp;
 
 public class EmailApp {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+
 }
