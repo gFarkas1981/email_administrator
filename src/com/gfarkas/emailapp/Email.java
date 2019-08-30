fae96fd943ef9905a8527374aea4163f3ff55882
gFarkas1981 <info@gfarkas.com>
2019-08-30 21:22:49 +0200

    Ask for Department - First Commit

--- src/com/gfarkas/emailapp/Email.java
+++ src/com/gfarkas/emailapp/Email.java
@@ -1,5 +1,7 @@
 package com.gfarkas.emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -17,11 +19,31 @@ public class Email {
 
         System.out.println("Email created: " + this.firstName + " " + this.lastName);
 
+        // Call a function asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        // Call a function that returns a random number
+
     }
 
     // Ask for department
+    private String setDepartment() {
+
+        System.out.print("Department codes\n\n1 for sales\n2 for Development\n3 for Accounting\n0 for none");
+        System.out.println();
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+
+        if (depChoice == 1) return "sales";
+        else if (depChoice == 2) return "dev";
+        else if (depChoice == 3) return "acct";
+        else return "";
+
+    }
 
     // Generate a random password
+
 
     // Set the mailbox capacity
 
