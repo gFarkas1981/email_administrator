555ee8f29cef6b4c954c9baad07945da644fb7c9
gFarkas1981 <info@gfarkas.com>
2019-08-30 18:48:44 +0200

    First Commit

--- src/com/gfarkas/emailapp/Email.java
+++ src/com/gfarkas/emailapp/Email.java
@@ -10,6 +10,14 @@ public class Email {
     private String alternateEmail;
 
     // Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        System.out.println("Email created: " + this.firstName + " " + this.lastName);
+
+    }
 
     // Ask for department
 
--- src/com/gfarkas/emailapp/EmailApp.java
+++ src/com/gfarkas/emailapp/EmailApp.java
@@ -3,7 +3,9 @@ package com.gfarkas.emailapp;
 public class EmailApp {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+
+        Email em1 = new Email("John", "Smith");
+
     }
 
 }
