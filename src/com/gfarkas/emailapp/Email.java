c93d47b02c9d53d7777af12be324073d93b8609b
gFarkas1981 <info@gfarkas.com>
2019-08-30 21:42:42 +0200

    randomPassword - First Commit

--- src/com/gfarkas/emailapp/Email.java
+++ src/com/gfarkas/emailapp/Email.java
@@ -7,6 +7,7 @@ public class Email {
     private String firstName;
     private String lastName;
     private String password;
+    private int defaultPasswordLength = 10;
     private String department;
     private int mailboxCapacity;
     private String alternateEmail;
@@ -23,7 +24,9 @@ public class Email {
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
 
-        // Call a function that returns a random number
+        // Call a function that returns a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
 
     }
 
@@ -43,6 +46,21 @@ public class Email {
     }
 
     // Generate a random password
+    private String randomPassword(int length) {
+
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%";
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++) {
+
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+
+        }
+
+        return new String(password);
+
+    }
 
 
     // Set the mailbox capacity
